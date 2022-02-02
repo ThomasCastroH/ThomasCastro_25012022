640b98980ccf65d4cfaa4bf9f5121bbaa7d04e77
Thomas Castro <contact.thomas.Castro@gmail.com>
2022-02-02 15:23:18 +0100

    allegement du code

--- scripts/displayTag.js
+++ scripts/displayTag.js
@@ -1,4 +1,4 @@
-function createBoxTags(category, content) {
+export default function createBoxTags(category, content) {
   const tagContainer = document.querySelector(".tagContainer");
 
   const tagBox = document.createElement("div");
@@ -16,25 +16,6 @@ function createBoxTags(category, content) {
   removeTag();
 }
 
-// Permet d'utiliser un seul addEventListener pour tous les tags
-[...document.querySelectorAll(".tags")].forEach(function(item) {
-  item.addEventListener("click", () => {
-    var targetClasses = event.target.classList[0];
-    var targetValue = event.target.textContent;
-    var targetClass = targetClasses.split('-');
-    var category = targetClass[1];
-
-    createBoxTags(category, targetValue);
-  })
-});
-
-function displayTag(value) {
-    const container = document.createElement("span");
-    container.setAttribute("class", "tag-box");
-    container.textContent = value;
-    return container;
-}
-  
 function removeTag() {
     const removeTag = document.querySelectorAll(".tag-box");
         if (removeTag.length) {
--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,5 @@
 import recipeFactory from "./displayRecipes.js";
+import createBoxTags from "./displayTag.js";
 
 function displayRecipes() {
     const recipesContainer = document.getElementById("recipe-list");
@@ -11,7 +12,19 @@ function displayRecipes() {
 }
 
 function init() {
-    displayRecipes();
+  // Permet d'utiliser un seul addEventListener pour tous les tags
+  [...document.querySelectorAll(".tags")].forEach(function(item) {
+    item.addEventListener("click", () => {
+      var targetClasses = event.target.classList[0];
+      var targetValue = event.target.textContent;
+      var targetClass = targetClasses.split('-');
+      var category = targetClass[1];
+
+      createBoxTags(category, targetValue);
+    })
+  });
+
+  displayRecipes();
 }
 
 init();

b0e65636fd3e9354574c0615c5e8538a1f4cdcca
Thomas Castro <contact.thomas.Castro@gmail.com>
2022-02-14 08:39:49 +0100

    recherche boucle native

--- scripts/research.js
+++ scripts/research.js
@@ -3,37 +3,38 @@ export { filterRecipes, filterRecipeByTags };
 function filterRecipes (recipes, string) {
 
     let input = string.toLowerCase();
-    
-    let filteredArray = recipes.filter(function (recipe) {
-        if (input.length >= 3){
+    let filteredArray = [];
 
-            recipe.ingredients.map(ingredienta => ingredienta.ingredient).map(ingr => {
-                    if (ingr.toLowerCase().includes(input)) {
-                        return recipe
-                    }
+    if (input.length >= 3) {
+        for (let i=0; i < recipes.length; i++) {
+            if (recipes[i].name.toLowerCase().includes(input)) {
+                filteredArray.push(recipes[i]);
+            }
+            for (let x=0; x < recipes[i].ingredients.length; x++) {
+                if (recipes[i].ingredients[x].ingredient.toLowerCase().includes(input)) {
+                    filteredArray.push(recipes[i]);                }
+            }
+            for (let y = 0; y < recipes[i].ustensils.length; y++) {
+                if (recipes[i].ustensils[y].toLowerCase().includes(input)) {
+                    filteredArray.push(recipes[i]);
                 }
-            );
-
-            recipe.ustensils.map(ustensil => {
-                if (ustensil.toLowerCase().includes(input)) {
-                    return recipe
-                }
-            });
-        
-            return recipe.name.toLowerCase().includes(input) || recipe.description.toLowerCase().includes(input); 
-
-        } else {
-            return recipes
+            }
+            if (recipes[i].appliance.toLowerCase().includes(input)) {
+                filteredArray.push(recipes[i]);
+            }
+            if (recipes[i].description.toLowerCase().includes(input)) {
+                filteredArray.push(recipes[i]);
+            }
         }
-    });
-
+    } else {
+        return recipes
+    }
+    console.log(filteredArray);
     let uniqueArray = [... new Set(filteredArray)];
     return uniqueArray;
 }
 
 function filterRecipeByTags (array) {
-
-    console.log(array);
 
     let recipes = document.querySelectorAll('.recipe');
     if (array.length == 0) {
